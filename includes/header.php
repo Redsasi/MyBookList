b96018d46c26a606a1220f7f16ebad982b1f152c
Saïam Canedo <redgames3000@gmail.com>
2024-08-15 16:14:22 +0200

    finition de style du header

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
     <header>
         <nav>
             <ul>
-                <li><a id="Icone" href="/index.php"><img src="images/Icone.png"/></a></li>
+                <li><a id="icone" href="/index.php"><img src="images/Icone.png"/></a></li>
                 <li><a href="#">My Books</a></li> <!--uniquement quand connecter en temps que lecteur -->
                 <li><a href="#">Search books</a></li> <!--tous le temps disponible même sans connection -->
                 <li><a href="#">Add Books</a></li><!--uniquement quand connecter en temps qu'administrateur -->
